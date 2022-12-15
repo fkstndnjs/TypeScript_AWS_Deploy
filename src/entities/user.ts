ca3aaec7f34de3c3abe19608c8a585d57d9168e5
fkstndnjs <fkstndnjs@naver.com>
2022-12-15 21:49:20 +0900

    fix

---
+++ src/entities/user.ts
@@ -0,0 +1,35 @@
+import { DataTypes } from "sequelize";
+import db from "../database";
+
+const User = db.define(
+  "user",
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    nickname: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default User;
