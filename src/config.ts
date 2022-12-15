e2a2aa3273f1b71400045bd183e8c0efbc2d0019
fkstndnjs <fkstndnjs@naver.com>
2022-12-15 22:05:58 +0900

    fix

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,12 @@ const config = {
     windowMs: parseInt(process.env.RATE_LIMIT_WINDOW_MS!),
     max: parseInt(process.env.RATE_LIMIT_MAX!),
   },
+  db: {
+    database: process.env.DATABASE,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+  },
 };
 
 export default config;
