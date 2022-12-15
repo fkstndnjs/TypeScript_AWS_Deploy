c8d2096e46b1d3f28d3e83029c9b515136997017
fkstndnjs <fkstndnjs@naver.com>
2022-12-15 15:51:30 +0900

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
 import config from "./config";
 
 const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(helmet());
 
 app.listen(config.port, () => {
   console.log("Server On...");
