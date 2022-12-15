9e21062b809e1d490ad3fa2744e4650041e7afc0
fkstndnjs <fkstndnjs@naver.com>
2022-12-15 16:01:23 +0900

    fix

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
 
 export default function validate(
   req: Request,
   res: Response,
   next: NextFunction
-) {}
+) {
+  const errors = validationResult(req);
+}
