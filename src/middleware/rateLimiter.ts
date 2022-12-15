a72ac748f848c9c8e70eec6341494bada3f93466
fkstndnjs <fkstndnjs@naver.com>
2022-12-15 16:11:56 +0900

    fix

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -1,3 +1,4 @@
+import { NextFunction, Request, Response } from "express";
 import limiter from "express-rate-limit";
 import config from "../config";
 
@@ -5,4 +6,7 @@ const rateLimiter = limiter({
   windowMs: config.rateLimit.windowMs,
   max: config.rateLimit.max,
   message: "잠시 후에 시도해주세요.",
+  handler: (req: Request, res: Response, next: NextFunction, options) => {
+    res.status(options.statusCode).send(options.message);
+  },
 });
