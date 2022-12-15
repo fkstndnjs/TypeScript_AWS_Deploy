91b3369ee127f4dd5c7d8ae5aa9afdd403b59e2a
fkstndnjs <fkstndnjs@naver.com>
2022-12-15 22:13:26 +0900

    fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import config from "../config";
 
 const createJWTToken = (id: number) => {
-  jwt.sign({ id });
+  const token = jwt.sign({ id }, config.jwt.secretKey, {
+    expiresIn: config.jwt.expiresIn,
+  });
+
+  return token;
 };
 
 export const signup = (req: Request, res: Response, next: NextFunction) => {};
