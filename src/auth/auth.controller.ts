abd0895f4a7701171fed69c01aab3051ded94c06
fkstndnjs <fkstndnjs@naver.com>
2022-12-15 22:08:59 +0900

    fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,3 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 
 export const signup = (req: Request, res: Response, next: NextFunction) => {};
+
+export const login = (req: Request, res: Response, next: NextFunction) => {};
