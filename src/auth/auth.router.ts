4c0f39b136baea8bef6cc130a30afd59ef348e08
fkstndnjs <fkstndnjs@naver.com>
2022-12-15 21:57:39 +0900

    fix

--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -1,6 +1,14 @@
 import express from "express";
+import { body } from "express-validator";
+import validate from "../middleware/validate";
 
 const authRouter = express.Router();
+const loginValidation = [
+  body("username").isEmpty().withMessage("아이디를 입력해주세요."),
+  body("password").isEmpty().withMessage("비밀번호를 입력해주세요"),
+  validate,
+];
+const signupValidation = [body("username")];
 
 authRouter.post("/signup", signupController);
 authRouter.post("/login", loginController);
