c1a42999311db440f4293177b60351cbb55174bd
fkstndnjs <fkstndnjs@naver.com>
2022-12-16 21:37:54 +0900

    fix

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -4,5 +4,8 @@ import jwt from "jsonwebtoken";
 const auth = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.get("Authorization");
 
-  if(authHeader&&authHeader.startsWith("Bearer "))
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+  } else {
+    res.status(401).send("잘못된 접근입니다.");
+  }
 };
