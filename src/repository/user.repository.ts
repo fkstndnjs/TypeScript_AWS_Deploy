e7dd0ec0856dcc032a9de84c1d87af79b719b605
fkstndnjs <fkstndnjs@naver.com>
2022-12-17 00:33:28 +0900

    fix

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -1,9 +1,5 @@
 import User from "../entities/user";
 
-export const getById = async (id: number) => {
-  const user = User.findByPk(id).then(console.log);
-};
-
 export const getByUsername = async (username: string) => {
   const user = User.findOne({
     where: {
