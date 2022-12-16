2c81496a8292fa0d10cb51f2f509990e0403f3e9
fkstndnjs <fkstndnjs@naver.com>
2022-12-16 22:03:03 +0900

    fix

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,18 +2,26 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import config from "../config";
 
-const auth = (req: Request, res: Response, next: NextFunction) => {
+const auth = (req: any, res: Response, next: NextFunction) => {
   const authHeader = req.get("Authorization");
 
   if (authHeader && authHeader.startsWith("Bearer ")) {
     const token = authHeader.split(" ")[1];
 
-    const temp = jwt.verify(token, config.jwt.secretKey, (err, data) => {
+    jwt.verify(token, config.jwt.secretKey, (err: any, data: any) => {
       if (err) {
         return res.status(401).send("토큰이 유효하지 않습니다.");
       }
+
+      if (!data) {
+        return res.status(401).send("토큰이 유효하지 않습니다.");
+      } else {
+        req.userId = data.id;
+
+        return next();
+      }
     });
   } else {
-    res.status(401).send("토큰이 유효하지 않습니다.");
+    return res.status(401).send("토큰이 유효하지 않습니다.");
   }
 };
