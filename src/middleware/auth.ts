2693c933d8d505154940917f5c010de81b0d9e83
fkstndnjs <fkstndnjs@naver.com>
2022-12-16 21:38:59 +0900

    fix

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -5,6 +5,9 @@ const auth = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.get("Authorization");
 
   if (authHeader && authHeader.startsWith("Bearer ")) {
+    const token = authHeader.split(" ")[1];
+
+    const temp = jwt.verify();
   } else {
     res.status(401).send("잘못된 접근입니다.");
   }
