c2c17ac0bab0871760678bc291099bb255f856d1
fkstndnjs <fkstndnjs@naver.com>
2022-12-16 21:35:44 +0900

    fix

---
+++ src/middleware/auth.ts
@@ -0,0 +1,6 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+const auth = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader;
+};
