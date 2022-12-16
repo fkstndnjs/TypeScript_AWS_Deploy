06eda4aa51b96540714c978b4a1182f8d11f27b5
fkstndnjs <fkstndnjs@naver.com>
2022-12-16 21:39:56 +0900

    fix

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import config from "../config";
 
 const auth = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.get("Authorization");
@@ -7,7 +8,7 @@ const auth = (req: Request, res: Response, next: NextFunction) => {
   if (authHeader && authHeader.startsWith("Bearer ")) {
     const token = authHeader.split(" ")[1];
 
-    const temp = jwt.verify();
+    const temp = jwt.verify(token, config.jwt.secretKey);
   } else {
     res.status(401).send("잘못된 접근입니다.");
   }
